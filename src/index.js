398148ca367f41c58853ccdac30d1ed22bcf497e
zhilingege <zhilingege@gmail.com>
2018-01-06 15:53:03 +0800

    完成《react小书》35章，但未做练习题

--- src/index.js
+++ src/index.js
@@ -7,76 +7,96 @@
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-function creactStore(state, stateChanger) {
+function creactStore(reducer) {
+   let state = null;
    const listeners = [];
    const subscribe = (listener) => listeners.push(listener);
    const getState = () => state;
    const dispatch = (action) => {
-      stateChanger(state,action);
-      listeners.forEach((listener)=>listener());
+      state = reducer(state, action);
+      listeners.forEach((listener) => listener());
    };
+   dispatch({});//初始化state
    return {getState, dispatch, subscribe};
 }
 
-function renderApp (newAppState,oldAppState={}) {
-   if( newAppState === oldAppState) return;
+function renderApp(newAppState, oldAppState = {}) {
+   if (newAppState === oldAppState) return;
    console.log('render app...');
    renderTitle(newAppState.title, oldAppState.title);
    renderContent(newAppState.content, oldAppState.content);
 }
 
-function renderTitle (newTitle,oldTitle={}) {
-   if( newTitle === oldTitle) return;
+function renderTitle(newTitle, oldTitle = {}) {
+   if (newTitle === oldTitle) return;
    console.log('render title...');
    const titleDOM = document.getElementById('title');
    titleDOM.innerHTML = newTitle.text;
    titleDOM.style.color = newTitle.color;
 }
 
-function renderContent (newContent,oldContent={}) {
-   if( newContent === oldContent) return;
+function renderContent(newContent, oldContent = {}) {
+   if (newContent === oldContent) return;
    console.log('render content...');
    const contentDOM = document.getElementById('content');
    contentDOM.innerHTML = newContent.text;
    contentDOM.style.color = newContent.color;
 }
 
-let appState = {
-   title: {
-      text: 'React.js 小书',
-      color: 'red',
-   },
-   content: {
-      text: 'React.js 小书内容',
-      color: 'blue'
+// let appState = {
+//    title: {
+//       text: 'React.js 小书',
+//       color: 'red',
+//    },
+//    content: {
+//       text: 'React.js 小书内容',
+//       color: 'blue'
+//    }
+// };
+
+function stateChanger(state, action) {//e.i. reducer 初始化和计算新的 statea
+   if (!state) {//初始化state
+      return {
+         title: {
+            text: 'React.js 小书',
+            color: 'red',
+         },
+         content: {
+            text: 'React.js 小书内容',
+            color: 'blue'
+         }
+      };
    }
-};
-
-
-
-
-
-
-function stateChanger(state, action) {//todo 这里还没写完， 《react小书34节》
    switch (action.type) {
       case 'UPDATE_TITLE_TEXT':
-         state.title.text = action.text;
-         break;
+         return {
+            ...state,
+            title: {
+               ...state.title,
+               text: action.text
+            },
+         };
       case 'UPDATE_TITLE_COLOR':
          state.title.color = action.color;
-         break;
+         return {
+            ...state,
+            title: {
+               ...state.title,
+               color: action.color
+            },
+         };
       default:
-         break;
+         return state;
    }
 }
 
-const store = creactStore(appState,stateChanger);
-let oldState = store.getState();//缓存旧的state
+const store = creactStore(stateChanger);
+let oldState = store.getState();//初始化的时候，缓存旧的state
 
-store.subscribe(()=>{//监听函数，只要调用了dispatch都会执行这个函数
-   const newState = store.getState();
-   renderApp(appState);
-   oldState = newState;
+store.subscribe(() => {//监听函数，只要调用了dispatch都会执行这个函数
+   const newState = store.getState();// 数据后续可能变化，获取新的 state
+   renderApp(newState, oldState);// 把新旧的 state 传进去渲染
+   oldState = newState;// 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
 });
 
 renderApp(store.getState()); // 首次渲染页面
